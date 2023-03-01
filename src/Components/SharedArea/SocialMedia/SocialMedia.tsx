02a0be1cba3cc9af4614c6b758448ed4eb6e7599
rotem bussi <112464919+Rotemkabir@users.noreply.github.com>
2023-03-01 17:39:00 +0200

    Change site icons color to black

--- src/Components/SharedArea/SocialMedia/SocialMedia.tsx
+++ src/Components/SharedArea/SocialMedia/SocialMedia.tsx
@@ -4,9 +4,9 @@ import { TiSocialFacebook, TiSocialLinkedin, TiSocialYoutube } from "react-icons
 function SocialMedia(): JSX.Element {
     return (
         <div className="SocialMedia">
-            <a href="https://www.linkedin.com/in/rotem-bussi-071ba5249/"><TiSocialLinkedin size={28}/></a>
-            <a href="https://www.youtube.com/watch?v=Nqgr-6Lte1E"><TiSocialYoutube size={28}/></a>
-            <a href="https://www.facebook.com/profile.php?id=100028315684952"><TiSocialFacebook size={28}/></a>  
+            <a href="https://www.linkedin.com/in/rotem-bussi-071ba5249/"><TiSocialLinkedin color="black" size={28}/></a>
+            <a href="https://www.youtube.com/watch?v=Nqgr-6Lte1E"><TiSocialYoutube color="black" size={28}/></a>
+            <a href="https://www.facebook.com/profile.php?id=100028315684952"><TiSocialFacebook color="black" size={28}/></a>  
         </div>
     );
 }
